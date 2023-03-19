7d7ba794ee8fe1fa9f3d783840d6ece697d06dc2
GYelejin <me@gy>
2023-03-19 12:41:16 +0300

    ko

--- calc.py
+++ calc.py
@@ -5,7 +5,7 @@ Reactor_power = 0
 
 
 def calculate_speed(Reactor_power, Ship_mass):
-    return 2 * np.divide(Reactor_power, 80) * np.divide(200, Ship_mass)
+    return 2 * (Reactor_power/ 80) * (200/Ship_mass)
 
 def calculate_growth_rate(autoclave_temperature, oxygen, SH):
     if oxygen // 60 >= SH:
@@ -13,8 +13,8 @@ def calculate_growth_rate(autoclave_temperature, oxygen, SH):
     else:
         oxygen_units = oxygen // SH
         return "dead"
-    return  np.sin(np.divide(np.pi, 2) + 
-                   np.divide(np.pi*(autoclave_temperature + 0.5 * oxygen_units), 40))
+    return  np.sin((np.pi/ 2) + 
+                   (np.pi*(autoclave_temperature + 0.5 * oxygen_units)/ 40))
 
 def calculate_new_population(old_population, growth_rate):
     return old_population + old_population * growth_rate
@@ -28,6 +28,10 @@ def calculate_amount_of_electricity(maintain_temperature):
     Одна единица SH не может усвоить более 60 единиц кислорода. 
     Минимальная популяция SH в автоклаве 8 единиц, меньшие популяции мгновенно вымирают.
 """
+
+def calculate_day(target_SH):
+    return np.log2(target_SH) - 2
+
 
 
 def main():
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from calc import calculate_speed
 import numpy as np
+from time import sleep
 """
 Перед стартом полёта закупочные цены таковы, что 10 единиц кислорода стоят столько же, 
 сколько 7 единиц ядерного топлива. 2 единицы кислорода стоят 14 кредитов.
@@ -30,46 +31,56 @@ import numpy as np
     
 """
 
+slow_T = 20
 
-
-def main():
+def calc_mission(target_SH, distance):
     #start 
     oxygen = 0
     nuclear_fuel = 0
     # target
-    SH = 504
-    distance = 38
+    #target_SH = 504
+    #distance = 38
     
     dictance_to_target = 0
     SH_population = 8
+    days = np.log2(target_SH) - 2
+    print(days)
+    need_oxygen = 0
+    
+    distance//2 - days
     day = 0
-    while dictance_to_target < distance and SH_population >= 8:
-        nuclear_fuel_for_SH = 0
-        nuclear_fuel_for_reactor = 0
+    speed_day = distance//2 - days 
+    while dictance_to_target < distance:
+        nuclear_fuel_for_SH = 36 // 11 + (36 % 11 == 0)
+        autoclave_temperature = 10
+        nuclear_fuel_for_reactor = 80
         # nuclear_fuel_for_SH + nuclear_fuel_for_reactor <= nuclear_fuel
         Ship_mass = 192 + SH_population
-        dictance_to_target -= calculate_speed(nuclear_fuel_for_reactor, Ship_mass)
-        
-        autoclave_temperature = sum(range(nuclear_fuel_for_SH + 1))
-        
-        if oxygen // 60 >= SH:
-            oxygen_units = 60
-        else:
-            oxygen_units = oxygen // SH
-            print("dead")
-            break
+        speed = calculate_speed(nuclear_fuel_for_reactor, Ship_mass)
+        print(speed)
+        dictance_to_target += int(speed) + 1
+        if speed <= 1 or SH_population >= target_SH:
+            autoclave_temperature = 21
+            nuclear_fuel_for_SH = 20
+        if speed_day <= day:
+            autoclave_temperature = 8
+        nuclear_fuel_for_SH = sum(range(autoclave_temperature + 1))
+        oxygen_units = 60
         oxygen -= oxygen_units * SH_population
-        growth_rate = np.sin(np.divide(np.pi, 2) + 
-                      np.divide(np.pi*(autoclave_temperature + 0.5 * oxygen_units), 40))
         
-        SH_population = SH_population * (growth_rate + 1 )
+        need_oxygen += oxygen_units * SH_population
+        growth_rate = np.sin(
+                        ((-np.pi) / 2) * 
+                        ((np.pi*(autoclave_temperature + 0.5 * oxygen_units)) / 40))
         
+        SH_population = SH_population * (float(growth_rate) + 1 )
         
-        
-        
-
-        day += 1
-    pass
+        nuclear_fuel += nuclear_fuel_for_SH + nuclear_fuel_for_reactor
+        #nuclear_fuel += 84
+        print(need_oxygen, dictance_to_target, int(SH_population))
+        sleep(1)
+    print(need_oxygen, nuclear_fuel, dictance_to_target, int(SH_population)*2)
+    return need_oxygen, nuclear_fuel
 
 if __name__ == '__main__':
-    main()
+    calc_mission(504, 36)
